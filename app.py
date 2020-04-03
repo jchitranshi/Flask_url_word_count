8cf096c64b62ab88d154a0ae929d4b29e14417f4
jchitranshi <jain.chixy005@gmail.com>
2020-04-03 17:41:36 +0530

    added hello_name

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app=Flask(__name__)
 def hello():
 	return "Hello!!"
 
+@app.route('/<name>')
+def hello_name(name):
+	return "Hello {}!".format(name)
+
 if __name__=='__main__':
 	app.run(debug=True)
 
