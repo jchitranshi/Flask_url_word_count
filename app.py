50e0070043e19b0bd9ade9bd52e7a494f6a53b53
jchitranshi <jain.chixy005@gmail.com>
2020-04-06 18:48:55 +0530

    add db details and logic to app.py and display

--- app.py
+++ app.py
@@ -1,24 +1,70 @@
 import os
-from flask import Flask
+import requests
+import operator
+import re
+import nltk
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
 
 app=Flask(__name__)
 app.config.from_pyfile('config.py')
 #app.config.from_object(os.environ['APP_SETTINGS'])
 #print(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///wordcount_dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:7742090748@localhost:5432/wordcount_dev'
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Result
 
-@app.route('/')
-def hello():
-	return "Hello!!"
+@app.route('/',methods=['GET','POST'])
+def index():
+	errors=[]
+	results={}
+	if request.method=='POST':
+		try:
+			url=request.form['url']
+			r=requests.get(url)
+			#print(r.text)
+		except:
+			errors.append("Unable to get URL. Please make sure its correct and try again!!")
+		if r:
+			#text_processing
+			raw=BeautifulSoup(r.text,'html.parser').get_text()
+			nltk.data.path.append('./nltk_data/')#set the path
+			tokens=nltk.word_tokenize(raw)
+			text=nltk.Text(tokens)
+			#remove punctuations 
+			nonPunct=re.compile('.*[A-Za-z].*')
+			raw_words=[w for w in text if nonPunct.match(w)]
+			raw_word_count=Counter(raw_words)
+			#stop_words
+			no_stop_words=[w for w in raw_words if w.lower() not in stops]
+			no_stop_words_count=Counter(no_stop_words)
+			#save_the_results
+			results=sorted(
+				no_stop_words_count.items(),
+				key=operator.itemgetter(1),
+				reverse=True
+				)[:10]
+			try:
+				result=Result(
+					url=url,
+					result_all=raw_word_count,
+					result_no_stop_words=no_stop_words_count
+					)
+				db.session.add(result)
+				db.session.commit()
+			except:
+				return render_template("Unable to add item to database.")
 
-@app.route('/<name>')
-def hello_name(name):
-	return "Hello {}!".format(name)
+	return render_template("index.html",errors=errors,results=results)
+
+# @app.route('/<name>')
+# def hello_name(name):
+# 	return "Hello {}!".format(name)
 
 if __name__=='__main__':
 	app.run(debug=True)
